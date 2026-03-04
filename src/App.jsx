4617bae7477f9c90a6d5f80b973935d220ca9db4
DarthKarim <darthkarim@proton.me>
2026-03-04 03:41:58 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,8 @@ function App() {
         </div>
 
         <div className="main-content" style={{ position: "relative", zIndex: 1 }}>
-          <h1>.karim</h1>
+          <h1>Hi, I'm Karim</h1>
+          <h2>(The one and only)</h2>
 
           <section className="dynamic-layout">
             <div className="dynamic-layout-item">Computer Science Student</div>
